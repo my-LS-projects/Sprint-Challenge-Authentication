96d00ef3a2549e1e6e500dfeee789b3892fa003c
frescocodes <hueydakotah@gmail.com>
2019-11-15 13:19:59 -0500

    feat: register and login complete

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,51 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const authMiddleware = require('./authenticate-middleware')
 
-router.post('/register', (req, res) => {
+const Users = require("../database/users/users-model");
+
+router.post("/register", (req, res) => {
   // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 12);
+
+  user.password = hash;
+  Users.add(user)
+    .then(user => res.status(201).json(user))
+    .catch(error => res.status(500).json({ error: "Error registering user" }));
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+  .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = getToken(user.id);
+        console.log(token);
+        res.status(200).json({ message: `welcome back, here's your token: `, token })
+      } else {
+        res.status(401).json({ error: "invalid credentials" })
+      }
+    })
+    
 });
+
+function getToken(id) {
+  const payload = {
+    id
+  };
+
+  const secret = process.env.JWT_SECRET || "rapper viper";
+
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,17 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization; 
+  const secret = process.env.JWT_SECRET || "rapper viper"; 
+
+  if (token) {
+    jwt.verify(token, secret, (error, decodedToken) => {
+      error ? res.status(401).json({ you: "shall not pass!" }) : next();
+    });
+  } else {
+    res.status(400).json({ message: "no credentials provided" });
+  }
 };
